0d76cf6182e7d177eac0c788b397b993eb62431c
Austin <95069137+AUSTIN0022@users.noreply.github.com>
2025-07-16 20:17:14 +0530

    Update updateAuctionsStatus.js

--- utils/worker/updateAuctionsStatus.js
+++ utils/worker/updateAuctionsStatus.js
@@ -1,7 +1,7 @@
 import cron from 'node-cron';
 import { Auction } from '../../model/DBModel.js';
 
-cron.schedule('*/10 * * * *', async () => {
+cron.schedule('*/5 * * * *', async () => {
   try {
     const now = new Date();
 
